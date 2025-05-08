bbe1a02fae9c2927698839882636b69876c9d39c
Este Hoyos <estehoyos@gmail.com>
2025-05-08 17:44:22 +0200

    feat: add mantine library support and basic appshell

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+        optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    }
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,36 +1,73 @@
+import '@mantine/core/styles.css';
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from 'next/navigation';
 import { routing } from "@/i18n/routing";
 import React from "react";
+import { Metadata } from 'next'
+import siteMetadata from "@/data/siteMetadata";
+import Shell from "@/components/shell";
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from "@mantine/core";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    default: siteMetadata.title,
+    template: `%s | ${siteMetadata.title}`,
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: './',
+    siteName: siteMetadata.title,
+    // images: [siteMetadata.socialBanner],
+    type: 'website',
+  },
+  // alternates: {
+  //   canonical: './',
+  //   types: {
+  //     'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+  //   },w
+  // },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     'max-video-preview': -1,
+  //     'max-image-preview': 'large',
+  //     'max-snippet': -1,
+  //   },
+  // },
+}
 
 export default async function LocaleLayout({
-    children,
-    params
+  children,
+  params
 }: {
-    children: React.ReactNode;
-    params: Promise<{ locale: string }>;
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-    const { locale } = await params;
-    if (!hasLocale(routing.locales, locale)) {
-        notFound();
-    }
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
 
-    return (
-        <html lang={locale}>
-            <body >
-                <NextIntlClientProvider>
-                    <header className="flex justify-between items-center">
-
-                    </header>
-                    <main>
-                        {children}
-                    </main>
-                    <footer>
-                        <p>Footer</p>
-                    </footer>
-                </NextIntlClientProvider>
-            </body>
-
-        </html>
-    )
+  return (
+    <html lang={locale}  {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body >
+        <NextIntlClientProvider>
+          <MantineProvider>
+            <Shell>
+              {children}
+            </Shell>
+          </MantineProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
 }
