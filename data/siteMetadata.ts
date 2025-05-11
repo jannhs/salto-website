9d2a4673358a141ef1e19eb21f3d87c7b7bb72f9
Este Hoyos <estehoyos@gmail.com>
2025-05-11 19:11:46 +0200

    feat: add site metadata

---
+++ data/siteMetadata.ts
@@ -0,0 +1,10 @@
+const siteMetadata = {
+    title: 'SALTO - Salute senza confini',
+    author: '',
+    headerTitle: 'SALTO',
+    description: 'Salute senza confini',
+    siteUrl: 'http://localhost:3000',
+
+}
+
+export default siteMetadata;
