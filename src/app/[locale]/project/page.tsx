bcd94fac718b5af4ddd586461444a778e2664d02
Este Hoyos <estehoyos@gmail.com>
2025-05-14 16:32:22 +0200

    temp: add first draft of project page

---
+++ src/app/[locale]/project/page.tsx
@@ -0,0 +1,9 @@
+export default function ProjectPage() {
+    return (
+        <>
+            <h1>Project </h1>
+            <p>This is the project page.</p>
+            <p>It is currently under construction.</p>
+        </>
+    );
+}
