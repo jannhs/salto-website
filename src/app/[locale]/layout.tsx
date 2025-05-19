a917e2f77fb1b5029b6ddd91cf76d656d1e700eb
Este Hoyos <estehoyos@gmail.com>
2025-05-19 16:51:05 +0200

    style: change primary color theme

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,7 +8,7 @@ import React from "react";
 import { Metadata } from "next";
 import siteMetadata from "@/data/siteMetadata";
 import Shell from "@/components/shell/shell";
-import { ColorSchemeScript, createTheme, MantineProvider, mantineHtmlProps } from "@mantine/core";
+import { ColorSchemeScript, createTheme, MantineProvider, mantineHtmlProps, MantineColorsTuple } from "@mantine/core";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -44,10 +44,59 @@ export const metadata: Metadata = {
   // },
 };
 
+// const palette = [
+//   "#F7BA2C",
+//   "#DCAF30",
+//   "#C0A334",
+//   "#A59838",
+//   "#898C3C",
+//   "#6E8140",
+//   "#527544",
+//   "#376A48",
+//   "#1B5E4C",
+//   "#005350",
+// ];
+
+const darkSlateGrey: MantineColorsTuple = [
+  "#f1f9f7",
+  "#e4efed",
+  "#c3dfd9",
+  "#9fcec5",
+  "#82c0b3",
+  "#6fb7a8",
+  "#63b3a3",
+  "#529d8e",
+  "#468c7e",
+  "#173630",
+];
+
+// https://mantine.dev/colors-generator/?color=dce9e2
+const lightSlateGreen: MantineColorsTuple = [
+  "#eef9f3",
+  "#dce9e2",
+  "#c4d8cd",
+  "#a4c2b2",
+  "#88b09a",
+  "#76a58b",
+  "#6c9f83",
+  "#5a8b70",
+  "#4e7c63",
+  "#3e6b53",
+];
+
 const theme = createTheme({
   fontFamily: "Inter, sans-serif",
   headings: { fontFamily: "Inter, sans-serif" },
-  primaryColor: "cyan",
+  colors: { darkSlateGrey, lightSlateGreen: lightSlateGreen },
+  primaryColor: "darkSlateGrey",
+  primaryShade: 6,
+  breakpoints: {
+    xs: "30em",
+    sm: "48em",
+    md: "64em",
+    lg: "74em",
+    xl: "90em",
+  },
 });
 
 export default async function LocaleLayout({
