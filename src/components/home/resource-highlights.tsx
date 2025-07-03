e0c09b672af0bc96803cb3872fd4a287d2d04ce4
Este Hoyos <estehoyos@gmail.com>
2025-07-03 19:54:33 +0200

    fix(home): change icons and uniform cards for resource highlights

--- src/components/home/resource-highlights.tsx
+++ src/components/home/resource-highlights.tsx
@@ -19,87 +19,80 @@ export default function ResourceHighlights() {
       title: t("Female reproductive health.title"),
       description: t("Female reproductive health.description"),
       image: "/icons/female-reproductive-health.png",
+      width: 140,
+      height: 140,
     },
     {
       title: t("Healthy diet.title"),
       description: t("Healthy diet.description"),
-      image: "/icons/healthy-diet.png",
+      image: "/icons/healthy-plate3.jpg",
+      width: 100,
+      height: 100,
     },
     {
       title: t("Correct use of medicines.title"),
       description: t("Correct use of medicines.description"),
       image: "/icons/medicines.png",
+      width: 100,
+      height: 100,
     },
-    {
-      title: t("Pregnancy.title"),
-      description: t("Pregnancy.description"),
-      image: "/icons/pregnancy.png",
-    },
+    // { // inserire dentro salute femminile
+    //   title: t("Pregnancy.title"),
+    //   description: t("Pregnancy.description"),
+    //   image: "/icons/pregnancy.png",
+    //   width: 100,
+    //   height: 100,
+    // },
     {
       title: t("Pediatric care.title"),
       description: t("Pediatric care.description"),
       image: "/icons/pediatric-care.png",
+      width: 100,
+      height: 100,
     },
     {
       title: t("Skin conditions.title"),
       description: t("Skin conditions.description"),
       image: "/icons/skin-conditions.png",
+      width: 100,
+      height: 100,
     },
     {
       title: t("Access to healthcare.title"),
       description: t("Access to healthcare.description"),
       image: "/icons/access-to-healthcare.png",
+      width: 100,
+      height: 100,
+    },
+    {
+      title: t("Prevention.title"),
+      description: t("Prevention.description"),
+      image: "/icons/prevention.png",
+      width: 170,
+      height: 170,
     },
   ];
 
-  // const resources = [
-  //   {
-  //     title: "Female reproductive health",
-  //     description: "Learn about female reproductive health.",
-  //     image: "/icons/female-reproductive-health.png",
-  //   },
-  //   {
-  //     title: "Healthy diet",
-  //     description: "Discover the importance of a balanced diet.",
-  //     image: "/icons/healthy-diet.png",
-  //   },
-  //   {
-  //     title: "Correct use of medicines",
-  //     description: "Understand how to use medicines safely.",
-  //     image: "/icons/medicines.png",
-  //   },
-  //   {
-  //     title: "Pregnancy",
-  //     description: "Get information on pregnancy and childbirth.",
-  //     image: "/icons/pregnancy.png",
-  //   },
-  //   {
-  //     title: "Pediatric care",
-  //     description: "Learn about pediatric care and child health.",
-  //     image: "/icons/pediatric-care.png",
-  //   },
-  //   {
-  //     title: "Skin conditions",
-  //     description: "Explore common skin conditions and treatments.",
-  //     image: "/icons/skin-conditions.png",
-  //   },
-  //   {
-  //     title: "Access to healthcare",
-  //     description: "Learn about healthcare access and services.",
-  //     image: "/icons/access-to-healthcare.png",
-  //   },
-  // ];
-
   const slides = resources.map((resource) => (
     <Carousel.Slide key={resource.title}>
-      <Card key={resource.title} className={classes.card} m={mobile ? "xs" : "md"} p={mobile ? "xs" : "lg"}>
+      <Card
+        key={resource.title}
+        className={classes.card}
+        m={mobile ? "xs" : "md"}
+        p={mobile ? "xs" : "lg"}
+        h="100%"
+        w={mobile ? "100%" : "350px"}
+        withBorder
+        radius="md"
+        shadow="sm"
+      >
         <Card.Section m="auto" pt="md">
-          <Image src={resource.image} height={100} alt={resource.title} width={100} />
+          <Image src={resource.image} height={resource.height} alt={resource.title} width={resource.width} />
         </Card.Section>
-        <Text fw="600" pt="sm" c="#3C776B" size="lg" ta="center">
+        <Text fw="600" pt="sm" c="#3C776B" size="xl" ta="center">
           {resource.title}
         </Text>
-        <Text size="md" mb="sm" ta="center">
+        <Text size="lg" mb="xs" ta="center">
           {resource.description}
         </Text>
         <Button variant="filled" w="130px" m="auto" color="#2e6940">
@@ -111,18 +104,18 @@ export default function ResourceHighlights() {
 
   return (
     <div className={classes.wrapper}>
-      <Title order={2} ta="center" mb="xs" c="#2D5A51">
+      <Title size={35} ta="center" mb="xs" c="#2D5A51">
         {t("title")}
       </Title>
       <Carousel
         withIndicators
-        height={345}
+        height={400}
         withControls
-        controlsOffset="sm"
-        controlSize={36}
+        controlsOffset="xs"
+        controlSize={50}
         nextControlIcon={
           <IconChevronRight
-            size={24}
+            size={40}
             color={theme.colors.darkSlateGreen[8]}
             stroke={1.5}
             className={classes.controlIcon}
@@ -130,14 +123,14 @@ export default function ResourceHighlights() {
         }
         previousControlIcon={
           <IconChevronLeft
-            size={24}
+            size={40}
             color={theme.colors.darkSlateGreen[8]}
             stroke={1.5}
             className={classes.controlIcon}
           />
         }
         slideSize={{ base: "100%", xs: "50%", sm: "50%", md: "33%", lg: "33%" }}
-        slideGap={{ base: "sm", sm: "sm", md: "md", lg: "md", xl: "md" }}
+        slideGap={{ base: "md", sm: "md", md: "md", lg: "md", xl: "md" }}
         emblaOptions={{ dragFree: true, align: "center" }}
         classNames={classes}
       >
