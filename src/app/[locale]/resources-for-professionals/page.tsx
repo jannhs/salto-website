785ac7d29d682eb531cae57660c2c564abb91c6f
Este Hoyos <estehoyos@gmail.com>
2025-07-15 19:31:14 +0200

    refactor: rename field of Resource type

--- src/app/[locale]/resources-for-professionals/page.tsx
+++ src/app/[locale]/resources-for-professionals/page.tsx
@@ -22,32 +22,32 @@ export default function ResourcesForProfessionalsPage() {
         <Title ta="center" order={2}>
           Statistiche sui migranti
         </Title>
-        <div className="px-5 py-5 m-auto max-w-360">
+        <div className="px-5 py-5 m-auto max-w-240 ">
           <Grid gutter="lg">
-            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+            <GridCol span={{ base: 12, sm: 6, md: 6, lg: 6 }}>
               <LinkCard
                 type="website"
                 title="Dati sull'immigrazione"
                 imagePreview="/previews/dati-immigrazione.png"
-                link="https://www.epicentro.iss.it/migranti/numeri-italia"
+                url="https://www.epicentro.iss.it/migranti/numeri-italia"
                 source="Istituto Superiore di Sanità"
                 sourceHref="https://www.epicentro.iss.it"
                 altPreview="Preview del sito dell'Istituto Superiore di Sanità"
               />
             </GridCol>
-            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+            <GridCol span={{ base: 12, sm: 6, md: 6, lg: 6 }}>
               <LinkCard
                 type="website"
                 title="Stato di salute dei migranti"
                 imagePreview="/previews/stato-salute-migranti.png"
-                link="http://www.epiprev.it/pubblicazione/epidemiol-prev-2017-41-3-
+                url="http://www.epiprev.it/pubblicazione/epidemiol-prev-2017-41-3-
 4-suppl-1"
                 source="Epidemiologia & Prevenzione - Rivista dell'Associazione italiana di epidemologia"
                 sourceHref="https://epiprev.it/"
                 altPreview="Preview del sito di Epidemiologia & Prevenzione"
               />
             </GridCol>
-            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+            {/* <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
               <LinkCard
                 type="pdf"
                 title="Tutela della salute"
@@ -57,7 +57,7 @@ export default function ResourcesForProfessionalsPage() {
                 sourceHref="https://epiprev.it/"
                 altPreview="Preview del sito della rete SAI"
               />
-            </GridCol>
+            </GridCol> 
             <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
               <LinkCard
                 type="pdf"
@@ -97,6 +97,75 @@ export default function ResourcesForProfessionalsPage() {
                 title="Controllo della tubercolosi"
                 imagePreview="/previews/controllo-tubercolosi.png"
                 link="https://www.epicentro.iss.it/migranti/LGTubercolosi"
+                source="Ministero della Salute"
+                sourceHref="http://www.salute.gov.it"
+                altPreview="Preview del sito del Ministero della Salute"
+              />
+            </GridCol>*/}
+          </Grid>
+        </div>
+        <Title ta="center" order={2} mt="sm">
+          Guida ai servizi destinati ai migranti
+        </Title>
+        <div className="px-5 py-5 m-auto max-w-240">
+          <Grid gutter="xl">
+            <GridCol span={{ base: 12, sm: 6, md: 6, lg: 6 }}>
+              <LinkCard
+                type="pdf"
+                title="Tutela della salute"
+                imagePreview="/previews/tutela-salute.png"
+                url="http://www.sprar.it/wp-content/uploads/2018/03/Dossier-TUTELA-DELLA-SALUTE-2018.pdf"
+                source="Rete Sistema Accoglienza Integrazione (SAI)"
+                sourceHref="https://epiprev.it/"
+                altPreview="Preview del sito della rete SAI"
+              />
+            </GridCol>
+            <GridCol span={{ base: 12, sm: 6, md: 6, lg: 6 }}>
+              <LinkCard
+                type="pdf"
+                title="Guida ai servizi di emergenza"
+                imagePreview="/previews/guida-servizi-emergenza.png"
+                url="https://www.epicentro.iss.it/migranti/pdf/Guida_ai_servizi_di_emergenza.pdf"
+                source="Istituto Superiore di Sanità"
+                sourceHref="https://www.epicentro.iss.it"
+                altPreview="Preview del sito dell'Istituto Superiore di Sanità"
+              />
+            </GridCol>
+          </Grid>
+        </div>
+        <Title ta="center" order={2} mt="sm">
+          Linee guida per la salute dei migranti
+        </Title>
+        <div className="px-5 pt-5 pb-12 m-auto max-w-360">
+          <Grid gutter="lg">
+            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+              <LinkCard
+                type="website"
+                title="Controlli sanitari all'arrivo"
+                imagePreview="/previews/controlli-sanitari-arrivo.png"
+                url="https://www.epicentro.iss.it/migranti/LGFrontiere"
+                source="Istituto Superiore di Sanità"
+                sourceHref="https://www.epicentro.iss.it"
+                altPreview="Preview del sito dell'Istituto Superiore di Sanità"
+              />
+            </GridCol>
+            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+              <LinkCard
+                type="pdf"
+                title="Assistenza e riabilitazione per i rifugiati"
+                imagePreview="/previews/assistenza-riabilitazione.png"
+                url="http://www.salute.gov.it/imgs/C_17_pubblicazioni_2599_allegato.pdf"
+                source="Ministero della Salute"
+                sourceHref="http://www.salute.gov.it"
+                altPreview="Preview del sito del Ministero della Salute"
+              />
+            </GridCol>
+            <GridCol span={{ base: 12, sm: 6, md: 4, lg: 4 }}>
+              <LinkCard
+                type="website"
+                title="Controllo della tubercolosi"
+                imagePreview="/previews/controllo-tubercolosi.png"
+                url="https://www.epicentro.iss.it/migranti/LGTubercolosi"
                 source="Ministero della Salute"
                 sourceHref="http://www.salute.gov.it"
                 altPreview="Preview del sito del Ministero della Salute"
