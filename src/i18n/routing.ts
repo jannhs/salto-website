6e294b37740fd874b1312d16c9f8ea7a81d840fd
Este Hoyos <estehoyos@gmail.com>
2025-07-16 10:12:16 +0200

    feat: add spanish and french messages and mdxs

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
-import {defineRouting} from 'next-intl/routing';
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ['it', 'en'],
+  locales: ["it", "en", "fr", "es"], // supported locales
 
-    defaultLocale: 'it', // used when no locale is set 
+  defaultLocale: "it", // used when no locale is set
 });
