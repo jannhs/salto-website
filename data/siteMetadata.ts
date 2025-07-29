2ad09b4292c2f35896f6563d7c7b7dfe6c991d64
Este Hoyos <estehoyos@gmail.com>
2025-07-29 12:22:26 +0200

    feat: update site metadata

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -1,10 +1,11 @@
 const siteMetadata = {
-    title: 'SALTO - Salute senza confini',
-    author: '',
-    headerTitle: 'SALTO',
-    description: 'Salute senza confini',
-    siteUrl: 'http://localhost:3000',
-
-}
+  title: "SALTO - Salute senza confini",
+  author: "Janneth Estefania Hoyos Rea",
+  headerTitle: "SALTO",
+  description: "A platform to promote public health initiatives and resources both by SALTO and third parties.",
+  siteUrl: "http://localhost:3000",
+  theme: "light",
+  siteRepo: "https://github.com/jannhs/salto-website",
+};
 
 export default siteMetadata;
