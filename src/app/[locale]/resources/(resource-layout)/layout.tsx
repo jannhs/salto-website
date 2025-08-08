02808b17b1494e1ec642d5c421303c918115433d
Este Hoyos <estehoyos@gmail.com>
2025-08-08 20:32:40 +0200

    temp: add breadcrumbs to resource routes

--- src/app/[locale]/resources/(resource-layout)/layout.tsx
+++ src/app/[locale]/resources/(resource-layout)/layout.tsx
@@ -1,12 +1,16 @@
 import { Text } from "@mantine/core";
 import { useTranslations } from "next-intl";
+import CustomBreadcrumbs from "@/components/resources/CustomBreadcrumbs";
 
 export default function ResourcesLayout({ children }: { children: React.ReactNode }) {
   const t = useTranslations("Resources");
 
   return (
     <div className="m-auto max-w-285 px-7 pt-5 pb-10">
-      <Text pb="md">{t("premiseThirdPartyResources")}</Text>
+      <CustomBreadcrumbs />
+      <Text pt="md" pb="md">
+        {t("premiseThirdPartyResources")}
+      </Text>
       {children}
     </div>
   );
